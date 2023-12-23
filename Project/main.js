6024d2064ca4f83054ae390a9916508dfbf892d8
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-12-23 15:52:13 +0000

    updation

--- Project/main.js
+++ Project/main.js
@@ -4,3 +4,17 @@ function openNav(){
    function closeNav(){
     document.getElementById("sidebar").style.width="0px";
    }
+
+   const track=document.querySelector(".carousel__track");
+
+const slides=Array.from(track.children);
+
+const nextbutton=document.querySelector(".carousel__button--right");
+
+const prevbutton=document.querySelector(".carousel__button--left");
+
+const dotsNav=document.querySelector(".carousel__nav");
+
+const dots=Array.from(dotsNav.children);
+
+const slideWidth=slides[0].getBoundingClientRect().width;
